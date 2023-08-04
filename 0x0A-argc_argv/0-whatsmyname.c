dc7bb1667931071525876e595de705122d308194
Jane <waguthijane61@gmail.com>
2023-08-04 02:21:00 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the programme name
+ * @agrc:is the  agrument count
+ * @agrv: the argument vector
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
