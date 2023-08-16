41e58f1dc1ac5761a3f5a90a9a98d61f0239b70e
Jane <waguthijane61@gmail.com>
2023-08-16 03:51:44 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,13 +5,14 @@
  * @array:array to search from
  * @size:size of array
  * @cmp:function pointer
+ * Return:-1 if not succefull
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array ==NULL || cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 	if (size <= 0)
 		return (-1);
